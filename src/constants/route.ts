e032a2d14527edf4f3e0a3c84044312c283eee3a
Ángel Luis Sánchez Pizarro <sanchezpizarro86@gmail.com>
2025-10-01 20:06:04 +0200

    feature/add-routing: feat: add router and pages

---
+++ src/constants/route.ts
@@ -0,0 +1,6 @@
+// Routes
+export const Route = {
+  Home: '/',
+  Podcast: '/podcast/:podcastId',
+  Episode: '/podcast/:podcastId/episode/:episodeId',
+} as const;
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,14 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+if (!globalThis.TextEncoder) {
+  (
+    globalThis as { TextEncoder?: typeof TextEncoder; TextDecoder?: typeof TextDecoder }
+  ).TextEncoder = TextEncoder;
+}
+
+if (!globalThis.TextDecoder) {
+  (
+    globalThis as { TextEncoder?: typeof TextEncoder; TextDecoder?: typeof TextDecoder }
+  ).TextDecoder = TextDecoder;
+}
